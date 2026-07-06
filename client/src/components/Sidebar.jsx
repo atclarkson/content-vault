83357887f580eb3f8222890781e14a68e626b997
atclarkson <atclarkson@gmail.com>
2026-07-06 22:00:41 +0700

    update logo

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,10 +1,11 @@
+import logo from "../assets/images/logo.png";
 import { NAV_ITEMS } from "../navItems";
 
 export default function Sidebar({ currentView, onNavigate }) {
   return (
-    <aside className="fixed left-0 top-0 hidden h-screen w-[240px] flex-col border-r border-stone-300/80 bg-stone-50/95 px-5 py-6 backdrop-blur lg:flex">
+    <aside className="fixed left-0 top-0 hidden h-screen w-[240px] flex-col border-r border-cyan-900/10 bg-white/90 px-5 py-6 backdrop-blur lg:flex">
       <div className="border-b border-stone-300/80 pb-6">
-        <p className="text-xs uppercase tracking-[0.35em] text-stone-500">content-vault</p>
+        <img src={logo} alt="AL Vault" className="h-10 w-auto" />
       </div>
 
       <nav className="mt-6 flex flex-col gap-2">
